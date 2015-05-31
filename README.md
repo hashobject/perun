fd5d8ea4c311d21f45af33715dff72d6a391a95a
Anton Podviaznikov <podviaznikov@gmail.com>
2015-05-31 13:59:38 -0700

    update readme. start work on collections

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # perun
+
+
+## Contributions
+
+We love contributions. Please submit your pull requests.
+
+
+## License
+
+Copyright © 2013-2015 Hashobject Ltd (team@hashobject.com).
+
+Distributed under the [Eclipse Public License](http://opensource.org/licenses/eclipse-1.0).
---
+++ src/io/perun/collection.clj
@@ -0,0 +1,38 @@
+(set-env!
+  :dependencies '[[org.clojure/clojure "1.6.0"]])
+
+(ns io.perun.collection
+  {:boot/export-tasks true}
+  (:require [boot.core       :as boot]
+            [boot.util       :as u]
+            [io.perun.utils  :as util]
+            [clojure.java.io :as io]))
+
+
+(def ^:private
+  +defaults+ {:target "public"
+              :datafile "posts.edn"})
+
+(boot/deftask collection
+  "Render collection files"
+  [o target   OUTDIR   str "The output directory"
+   d datafile DATAFILE str "Datafile with all parsed meta information"
+   r renderer RENDERER sym "Page renderer"]
+  (let [tmp (boot/temp-dir!)]
+    (fn middleware [next-handler]
+      (fn handler [fileset]
+        (let [options (merge +defaults+ *opts*)
+              posts (util/read-posts fileset (:datafile options))]
+           (doall
+            (map
+              (fn [post]
+                (let [render-fn (resolve renderer)
+                      html (render-fn post)
+                      post-file-path (str (:target options) "/"
+                                          (or (:filepath post)
+                                              (str (:filename post) ".html")))]
+                  (util/create-file tmp post-file-path html)))
+            posts))
+          (u/info (str "Render all pages/posts\n"))
+          (util/commit-and-next fileset tmp next-handler))))))
+
