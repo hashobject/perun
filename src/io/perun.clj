3b07a95b171fc48d31c8301d24ae53b8770a8462
Martin Klepsch <martinklepsch@googlemail.com>
2015-06-19 00:19:18 +0200

    add slug task to io.perun ns

--- src/io/perun.clj
+++ src/io/perun.clj
@@ -3,6 +3,7 @@
   (:require [boot.core :as boot :refer [deftask]]
             [boot.pod :as pod]
             [boot.util :as u]
+            [clojure.string :as string]
             [io.perun.core :as perun]))
 
 (def ^:private global-deps
@@ -71,6 +72,27 @@
           fs-with-meta (with-meta fileset {:metadata updated-files})]
       (u/info "Remove draft files. Remaining %s files\n" (count updated-files))
       fs-with-meta)))
+
+(defn ^:private default-slug-fn [filename]
+  (->> (string/split filename #"[-\.]")
+       (drop 3)
+       drop-last
+       (string/join "-")
+       string/lower-case))
+
+(deftask slug
+  "Adds :slug key to files metadata. Slug is derived from filename."
+  [s slug-fn SLUGFN sym "Function to build slug from filename"]
+  (boot/with-pre-wrap fileset
+    (let [slug-fn       (or slug-fn default-slug-fn)
+          files         (:metadata (meta fileset))
+          updated-files (into {}
+                              (for [[f m] files]
+                                [f (assoc m :slug (slug-fn f))]))]
+      (u/dbug "Generated Slugs:\n%s\n"
+              (pr-str (map :slug (vals updated-files))))
+      (u/info "Added slugs to %s files\n" (count updated-files))
+      (with-meta fileset {:metadata updated-files}))))
 
 (defn ^:private default-permalink-fn [metadata]
   (str  "/" (:slug metadata) "/index.html"))
