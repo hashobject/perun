947d6f817cf434a548bec9b45a4f76a09cc3bc20
Juho Teperi <juho.teperi@iki.fi>
2015-06-20 11:45:04 +0300

    Add tests for filepath and url functions

---
+++ test/io/perun/core_test.clj
@@ -0,0 +1,27 @@
+(ns io.perun.core-test
+  (:require [clojure.test :refer :all]
+            [io.perun.core :refer :all]
+            [clojure.java.io :as io]))
+
+(deftest absolutize-url-test
+  (is (= "/index.html" (absolutize-url "index.html")))
+  (is (= "/index.html" (absolutize-url "/index.html")))
+  )
+
+(deftest relativize-url-test
+  (is (= "index.html" (relativize-url "index.html")))
+  (is (= "index.html" (relativize-url "/index.html")))
+  )
+
+(deftest create-filepath-test
+  (is (= (.getPath (io/file "index.html"))
+         (create-filepath "index.html")))
+  (is (= (.getPath (io/file "public" "index.html"))
+         (create-filepath "public" "index.html")))
+  )
+
+(deftest url-to-path-test
+  (is (= (.getPath (io/file "index.html")) (url-to-path "index.html")))
+  (is (= (.getPath (io/file "public" "index.html")) (url-to-path "public/index.html")))
+  (is (= (.getPath (io/file "public" "index.html")) (url-to-path "public//index.html")))
+  )
