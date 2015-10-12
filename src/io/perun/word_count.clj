75c97423cfe21e6acf0c720bfc55a90e6ea4a4cc
Anton Podviaznikov <podviaznikov@gmail.com>
2015-10-11 22:54:27 -0700

    prevent null pointers

--- src/io/perun/word_count.clj
+++ src/io/perun/word_count.clj
@@ -2,9 +2,12 @@
   (:require [boot.util         :as u]
             [io.perun.core     :as perun]))
 
+(defn add-word-count [file]
+  (if-let [content (:content file)]
+    (assoc file :word-count (count (clojure.string/split content #"\s")))
+    file))
 
 (defn count-words [files]
-  (let [add-wc #(assoc % :word-count (count (clojure.string/split (:content %) #"\s")))
-        updated-files (map add-wc files)]
+  (let [updated-files (map add-word-count files)]
     (u/info "Added word-count to %s files\n" (count updated-files))
     updated-files))
