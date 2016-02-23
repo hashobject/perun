ba4c139c7d91137c7f6ac9e012495714d1181888
Juho Teperi <juho.teperi@iki.fi>
2016-02-23 17:25:11 +0200

    Update atom and rss tasks to use correct options names

--- src/io/perun.clj
+++ src/io/perun.clj
@@ -323,9 +323,9 @@
   [f filename    FILENAME    str  "generated RSS feed filename"
    _ filterer    FILTER      code "predicate to use for selecting entries (default: `:content`)"
    o out-dir     OUTDIR      str  "the output directory"
-   t title       TITLE       str  "feed title. Default to global :site-title"
-   p description DESCRIPTION str  "feed description. Default to global :description"
-   l link        LINK        str  "feed site link. Default to global :base-url"]
+   t site-title  TITLE       str  "feed title"
+   p description DESCRIPTION str  "feed description"
+   l base-url    LINK        str  "feed link"]
   (let [pod (create-pod rss-deps)
         tmp (boot/tmp-dir!)]
     (boot/with-pre-wrap fileset
@@ -350,10 +350,10 @@
   [f filename    FILENAME    str  "generated Atom feed filename"
    _ filterer    FILTER      code "predicate to use for selecting entries (default: `:content`)"
    o out-dir     OUTDIR      str  "the output directory"
-   t title       TITLE       str  "feed title. Default to global :site-title"
+   t site-title  TITLE       str  "feed title"
    s subtitle    SUBTITLE    str  "feed subtitle"
-   p description DESCRIPTION str  "feed description. Default to global :description"
-   l link        LINK        str  "feed site link. Default to global :base-url"]
+   p description DESCRIPTION str  "feed description"
+   l base-url    LINK        str  "feed link"]
   (let [pod (create-pod atom-deps)
         tmp (boot/tmp-dir!)]
     (boot/with-pre-wrap fileset
