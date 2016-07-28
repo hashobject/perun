3ba1cbdc4bee72cea86bc7c19781172007f66e41
Juho Teperi <juho.teperi@iki.fi>
2016-07-28 15:36:36 +0300

    Fix author-email

--- src/io/perun/atom.clj
+++ src/io/perun/atom.clj
@@ -33,7 +33,7 @@
        [:id base-url]
        (for [{:keys [uuid canonical-url content name author author-email] :as post} (take 10 posts)
              :let [author (or author (:name (:author global-metadata)))
-                   author-email (or author (:email (:author global-metadata)))]]
+                   author-email (or author-email (:email (:author global-metadata)))]]
          (do
            (assert (seq uuid) (format "Atom XML requires that each post has a unique uuid, if you need one, use this: %s" (str (java.util.UUID/randomUUID))))
            (assert (seq author) (format "Atom XML requires that each post has author name."))
